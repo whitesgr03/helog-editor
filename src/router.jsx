24aec0953cb23bed9e2bc2112557b287890b2b0a
Weiss Bai <whitesgr03@gmail.com>
2024-07-15 15:24:25 +0800

    Refactored router

---
+++ src/router.jsx
@@ -0,0 +1,57 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./styles/index.css";
+
+import UserProvider from "./components/UserProvider";
+import Error from "./components/layout/Error";
+import NotFound from "./components/layout/NotFound";
+import Dashboard from "./components/Dashboard";
+import App from "./components/App";
+import PostCreate from "./components/PostCreate";
+import PostUpdate from "./components/PostUpdate";
+import Callback from "./components/Callback";
+
+const router = () => (
+	<RouterProvider
+		router={createBrowserRouter([
+			{
+				path: "/",
+				element: <UserProvider />,
+				children: [
+					{
+						path: "/",
+						element: <App />,
+						children: [
+							{
+								path: "*",
+								element: <NotFound />,
+							},
+							{
+								index: true,
+								element: <Dashboard />,
+							},
+							{
+								path: "error",
+								element: <Error />,
+							},
+							{
+								path: "/create",
+								element: <PostCreate />,
+							},
+							{
+								path: "/update/:postId",
+								element: <PostUpdate />,
+							},
+						],
+					},
+					{
+						path: "/callback",
+						element: <Callback />,
+					},
+				],
+			},
+		])}
+	/>
+);
+
+export default router;
