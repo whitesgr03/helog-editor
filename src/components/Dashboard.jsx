638a39eb347420816691ab5af2975fa0e705383e
Weiss Bai <whitesgr03@gmail.com>
2024-07-15 20:11:21 +0800

    Updated Dashboard component
    Use TableRows component to render all table rows.
    
    Added new post link.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,45 @@
+// Package
+import { Link } from "react-router-dom";
+
 // Styles
 import style from "../styles/Dashboard.module.css";
+import button from "../styles/utils/button.module.css";
 
-// Variables
-import imageUrl from "../assets/bram-naus-n8Qb1ZAkK88-unsplash.jpg";
+// Component
+import TableRows from "./TableRows";
 
 const Dashboard = () => {
 	const posts = [
 		{
+			_id: 0,
 			title: "This is title",
 			publish: false,
 			lastModified: new Date(),
 			createdAt: new Date(),
 		},
 		{
+			_id: 1,
+			title: "This is title2",
+			publish: true,
+			lastModified: new Date(),
+			createdAt: new Date(),
+		},
+		{
+			_id: 2,
+			title: "This is title2",
+			publish: true,
+			lastModified: new Date(),
+			createdAt: new Date(),
+		},
+		{
+			_id: 3,
+			title: "This is title2",
+			publish: true,
+			lastModified: new Date(),
+			createdAt: new Date(),
+		},
+		{
+			_id: 4,
 			title: "This is title2",
 			publish: true,
 			lastModified: new Date(),
@@ -20,34 +47,37 @@ const Dashboard = () => {
 		},
 	];
 
-	const item = posts.map(post => {
-		return (
-			<li key={post._id}>
-				<div>
-					<img src={imageUrl} />
-				</div>
-				<div>{post.title}</div>
-				<div>{post.publish}</div>
-				<div>
-					<button>Edit</button>
-					<button>Delete</button>
-				</div>
-			</li>
-		);
+	const trs = posts.map(post => {
+		return <TableRows key={post._id} post={post} />;
 	});
 	return (
 		<div className={style.dashboard}>
-			<h3>Post dashboard</h3>
+			<h3>Posts Dashboard</h3>
+			<div className={style.buttonWrap}>
+				<span>
+					{posts.length > 0 && `Total posts: ${posts.length}`}
+				</span>
+				<Link to="/posts/create" className={button.success}>
+					New Post
+				</Link>
+			</div>
 
-			<ul>
-				<li>
-					<div>Image</div>
-					<div>Title</div>
-					<div>Publisher</div>
-					<div></div>
-				</li>
-				{item}
-			</ul>
+			<div className={style.container}>
+				{posts.length > 0 ? (
+					<table>
+						<tr>
+							<th>Title</th>
+							<th>Publish</th>
+							<th>Last Modified</th>
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+						{trs}
+					</table>
+				) : (
+					<p>There are not posts.</p>
+				)}
+			</div>
 		</div>
 	);
 };
