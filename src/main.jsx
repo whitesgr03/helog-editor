72784bcc0bb1355b6cda19b23d0e3d362cf4418e
Weiss Bai <whitesgr03@gmail.com>
2024-07-15 15:24:39 +0800

    Format code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Router from "./router.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Router />
+	</React.StrictMode>
+);
