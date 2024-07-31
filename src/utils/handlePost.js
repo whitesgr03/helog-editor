fda75fb10ef679e9b055e4c22a524b1778130d61
Weiss Bai <whitesgr03@gmail.com>
2024-07-31 20:02:02 +0800

    Refactored handlePost

--- src/utils/handlePost.js
+++ src/utils/handlePost.js
@@ -1,37 +1,59 @@
-// import handleFetch from "./handleFetch";
+import handleFetch from "./handleFetch";
 
-// const url = `${import.meta.env.VITE_RESOURCE_ORIGIN}/blog/comments`;
+const url = `${import.meta.env.VITE_RESOURCE_ORIGIN}/blog/posts`;
 
-// const createComment = async ({ token, data }) => {
-// 	const options = {
-// 		method: "POST",
-// 		headers: {
-// 			Authorization: `Bearer ${token}`,
-// 			"Content-Type": "application/json",
-// 		},
-// 		body: JSON.stringify(data),
-// 	};
-// 	return await handleFetch(url, options);
-// };
-// const updateComment = async ({ token, data, commentId }) => {
-// 	const options = {
-// 		method: "PUT",
-// 		headers: {
-// 			Authorization: `Bearer ${token}`,
-// 			"Content-Type": "application/json",
-// 		},
-// 		body: JSON.stringify(data),
-// 	};
-// 	return await handleFetch(`${url}/${commentId}`, options);
-// };
-// const deleteComment = async ({ token, commentId }) => {
-// 	const options = {
-// 		method: "DELETE",
-// 		headers: {
-// 			Authorization: `Bearer ${token}`,
-// 		},
-// 	};
-// 	return await handleFetch(`${url}/${commentId}`, options);
-// };
+const getPosts = async ({ signal, userId }) => {
+	const options = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		signal,
+	};
+	return await handleFetch(`${url}?userId=${userId}`, options);
+};
+const getPost = async ({ postId }) => {
+	const options = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	const result = await handleFetch(`${url}/${postId}`, options);
 
-// export { createComment, updateComment, deleteComment };
+	return result;
+};
+const createPost = async ({ token, data }) => {
+	const options = {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	};
+
+	return await handleFetch(url, options);
+};
+const updatePost = async ({ token, data, postId }) => {
+	const options = {
+		method: "PUT",
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	};
+	return await handleFetch(`${url}/${postId}`, options);
+};
+const deletePost = async ({ token, postId }) => {
+	const options = {
+		method: "DELETE",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+	return await handleFetch(`${url}/${postId}`, options);
+};
+
+export { getPosts, getPost, createPost, updatePost, deletePost };
