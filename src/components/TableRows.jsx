37aadc7b94035a57761df61b8b64aa241b5e45f4
Weiss Bai <whitesgr03@gmail.com>
2024-08-01 00:24:03 +0800

    Refactored TableRows component
    Received onGetPosts, publishing and onPublishing props.
    
    Received outlet context states.
    
    Created handleUpdatePublish function to switch publish state of post.
    
    Create a handleActiveModel function to pass DeletePostModel to the
    model component, and then the model component will render the
    DeletePostModel and handle deleting post.
    
    The first of td uses dangerouslySetInnerHTML to render escaped post
    title.
    
    The button in the third td is replaced with link that navigate to
    the editor.
    
    Added the last td with loadIcon class.

--- src/components/TableRows.jsx
+++ src/components/TableRows.jsx
@@ -1,5 +1,6 @@
 // Packages
 import { useState } from "react";
+import { useOutletContext, Link } from "react-router-dom";
 import { format } from "date-fns";
 import PropTypes from "prop-types";
 
@@ -7,19 +8,103 @@ import PropTypes from "prop-types";
 import style from "../styles/TableRows.module.css";
 import image from "../styles/utils/image.module.css";
 
-const TableRows = ({ post }) => {
-	const [publish, setPublish] = useState(false);
+// Components
+import DeletePostModel from "./layout/DeletePostModel";
 
-	const handleSwitchPublish = () => setPublish(!publish);
+// Utils
+import { updatePost, deletePost } from "../utils/handlePost";
+
+const TableRows = ({ post, onGetPosts, publishing, onPublishing }) => {
+	const {
+		accessToken,
+		onVerifyTokenExpire,
+		onExChangeToken,
+		onModel,
+		onAlert,
+	} = useOutletContext();
+	const [loading, setLoading] = useState(false);
+
+	const postState = {
+		postId: post._id,
+		publish: post.publish,
+		data: {
+			content: post.content ?? "",
+			title: post.title ?? "",
+			mainImage: post.mainImage ?? "",
+		},
+	};
+
+	const handleUpdatePublish = async () => {
+		const handleUpdate = async () => {
+			onPublishing(true);
+			setLoading(true);
+
+			const isTokenExpire = await onVerifyTokenExpire();
+			const newAccessToken = isTokenExpire && (await onExChangeToken());
+
+			const result = await updatePost({
+				token: newAccessToken || accessToken,
+				data: { publish: !post.publish },
+				postId: post._id,
+			});
+
+			const handleSuccess = async () => {
+				await onGetPosts();
+				onAlert({
+					message: `${!post.publish ? "Published" : "Unpublished"} ${
+						post.title
+					} `,
+					error: false,
+				});
+			};
+
+			result.success
+				? await handleSuccess()
+				: onAlert({ message: result.message, error: true });
+
+			setLoading(false);
+			onPublishing(false);
+		};
+
+		!publishing && (await handleUpdate());
+	};
+
+	const handleActiveModel = () => {
+		const handleDelete = async () => {
+			const isTokenExpire = await onVerifyTokenExpire();
+			const newAccessToken = isTokenExpire && (await onExChangeToken());
+
+			const result = await deletePost({
+				token: newAccessToken || accessToken,
+				postId: post._id,
+			});
+
+			const handleSuccess = async () => {
+				await onGetPosts();
+				onAlert({ message: `Deleted ${post.title}`, error: false });
+			};
+
+			result.success
+				? await handleSuccess()
+				: onAlert({ message: result.message, error: true });
+
+			onModel(null);
+		};
+
+		onModel(<DeletePostModel onDelete={handleDelete} title={post.title} />);
+	};
+
 	return (
-		<tr className={style.tableRows}>
+		<tr className={`${style.tableRows} ${loading ? style.loading : ""}`}>
 			<td title={post.title}>
-				<span>{post.title}</span>
+				<span dangerouslySetInnerHTML={{ __html: post.title }} />
 			</td>
 			<td>
 				<button
-					className={`${style.switch} ${publish ? style.active : ""}`}
-					onClick={handleSwitchPublish}
+					className={`${style.switch} ${
+						post.publish ? style.active : ""
+					}`}
+					onClick={handleUpdatePublish}
 				>
 					<div>
 						<div />
@@ -30,25 +115,29 @@ const TableRows = ({ post }) => {
 				<span>{format(post.lastModified, "MMMM d, y")}</span>
 			</td>
 			<td className={style.buttonWrap}>
-				<button
-				// onClick={handleShowEditBox}
-				>
+				<Link to="/post/editor" state={postState}>
 					<span className={`${image.icon} ${style.edit}`} />
-				</button>
+				</Link>
 			</td>
 			<td className={style.buttonWrap}>
-				<button
-				// onClick={handleDelete}
-				>
+				<button onClick={handleActiveModel}>
 					<span className={`${image.icon} ${style.delete}`} />
 				</button>
 			</td>
+			{loading && (
+				<td className={style.loadIcon}>
+					<span className={`${image.icon} ${style.load}`} />
+				</td>
+			)}
 		</tr>
 	);
 };
 
 TableRows.propTypes = {
 	post: PropTypes.object,
+	onGetPosts: PropTypes.func,
+	publishing: PropTypes.bool,
+	onPublishing: PropTypes.func,
 };
 
 export default TableRows;
