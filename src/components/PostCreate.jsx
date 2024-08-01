7a8f6274fee20668c58d20a13fd6ea239d893a22
Weiss Bai <whitesgr03@gmail.com>
2024-08-01 11:31:04 +0800

    Refactored PostCreate component
    Added handlePublish to publish post.
    
    Added useEffect to call handleSetPost to automatically create
    or update a post two seconds after any input text.
    
    Use tinyMCE Editor component to handle post title, image and content.

--- src/components/PostCreate.jsx
+++ src/components/PostCreate.jsx
@@ -1,5 +1,353 @@
+// Packages
+import { useState, useRef, useEffect } from "react";
+import {
+	useOutletContext,
+	useNavigate,
+	useLocation,
+	Link,
+} from "react-router-dom";
+import { Editor } from "@tinymce/tinymce-react";
+
+// Styles
+import button from "../styles/utils/button.module.css";
+import style from "../styles/PostCreate.module.css";
+import image from "../styles/utils/image.module.css";
+
+// Utils
+import { createPost, updatePost } from "../utils/handlePost";
+
+// Components
+import Loading from "./layout/Loading";
+
+// Variables
+const editorContentInit = {
+	selection_toolbar: {
+		quickbars_selection_toolbar: "bold italic link blocks blockquote",
+		link_default_target: "_blank",
+		link_assume_external_targets: "https",
+		link_context_toolbar: true,
+		link_title: false,
+		block_formats: "Large=h3; Normal=p; Small=h5",
+	},
+	insert_toolbar: {
+		quickbars_insert_toolbar: "bullist numlist image codesample hr",
+		image_uploadtab: false,
+		typeahead_urls: false,
+	},
+	image_toolbar: {
+		quickbars_image_toolbar: "image",
+	},
+};
+
+const defaultData = {
+	title: "",
+	mainImage: "",
+	content: "",
+};
+
 const PostCreate = () => {
-	return <div>This is PostCreate</div>;
+	const {
+		darkTheme,
+		accessToken,
+		onVerifyTokenExpire,
+		onExChangeToken,
+		onAlert,
+	} = useOutletContext();
+	const navigate = useNavigate();
+	const { state } = useLocation();
+
+	const [previousData, setPreviousData] = useState(
+		state?.data ?? defaultData
+	);
+
+	const [data, setData] = useState(state?.data ?? defaultData);
+	const [firstCreatePostId, setFirstCreatePostId] = useState(null);
+
+	const [publishing, setPublishing] = useState(false);
+	const [saving, setSaving] = useState(null);
+
+	const [loadCount, setLoadCount] = useState(0);
+
+	const timer = useRef(null);
+
+	const contentRef = useRef(null);
+	const titleRef = useRef(null);
+	const mainImageRef = useRef(null);
+
+	const handlePublish = async () => {
+		const handleUpdate = async () => {
+			setPublishing(true);
+			const isTokenExpire = await onVerifyTokenExpire();
+			const newAccessToken = isTokenExpire && (await onExChangeToken());
+
+			const result = await updatePost({
+				token: newAccessToken || accessToken,
+				data: { publish: true },
+				postId: firstCreatePostId || state.postId,
+			});
+
+			const handleSuccess = () => {
+				onAlert({
+					message: `${data.title} Published`,
+					error: false,
+				});
+				navigate("/");
+			};
+
+			result.success
+				? handleSuccess()
+				: onAlert({ message: result.message, error: true });
+
+			setPublishing(false);
+		};
+
+		!saving && (await handleUpdate());
+	};
+
+	useEffect(() => {
+		const handleSetPost = async () => {
+			onAlert({ message: "saving post..." });
+			setSaving(true);
+			setPreviousData(data);
+			const obj = {};
+			data.title !== previousData.title && (obj.title = data.title);
+			data.mainImage !== previousData.mainImage &&
+				(obj.mainImage = data.mainImage);
+			data.content !== previousData.content &&
+				(obj.content = data.content);
+
+			const isTokenExpire = await onVerifyTokenExpire();
+			const newAccessToken = isTokenExpire && (await onExChangeToken());
+
+			const result =
+				firstCreatePostId || state?.postId
+					? await updatePost({
+							token: newAccessToken || accessToken,
+							data: obj,
+							postId: firstCreatePostId || state?.postId,
+					  })
+					: await createPost({ token: accessToken, data: obj });
+
+			result.success
+				? !firstCreatePostId &&
+				  !state?.postId &&
+				  setFirstCreatePostId(result.data.post.id)
+				: onAlert({ message: result.message, error: true });
+
+			setSaving(false);
+			onAlert({ message: "saved post" });
+		};
+
+		!saving &&
+			JSON.stringify(data) !== JSON.stringify(previousData) &&
+			JSON.stringify(data) !== JSON.stringify(defaultData) &&
+			(timer.current = setTimeout(() => handleSetPost(), 2000));
+
+		return () => {
+			clearTimeout(timer.current);
+		};
+	}, [
+		firstCreatePostId,
+		state,
+		saving,
+		data,
+		previousData,
+		accessToken,
+		onVerifyTokenExpire,
+		onExChangeToken,
+		onAlert,
+	]);
+
+	return (
+		<div id={"postEditor"} className={style.postCreate}>
+			<div className={style.buttonWrap}>
+				<Link to="/" className={style.link}>
+					<span className={`${style.leftArrow} ${image.icon}`} />
+					Back to Dashboard
+				</Link>
+				{(firstCreatePostId || state?.publish === false) && (
+					<button className={button.success} onClick={handlePublish}>
+						{publishing ? "Publishing" : "Publish"}
+					</button>
+				)}
+			</div>
+			<div className={style.container}>
+				<div
+					className={`${style.editors}  ${
+						loadCount < 3 ? style.loading : ""
+					}`}
+				>
+					<Editor
+						id="editorTitle"
+						key={darkTheme}
+						apiKey="x2zlv8pvui3hofp395wp6my8308b15h3s176scf930dizek1"
+						onInit={(_evt, editor) => {
+							loadCount < 3 &&
+								setLoadCount(loadCount => loadCount + 1);
+							titleRef.current = editor;
+							JSON.stringify(data) ===
+								JSON.stringify(defaultData) && editor.focus();
+						}}
+						tagName="h2"
+						onEditorChange={(value, editor) => {
+							const wordCountLimit = 100;
+							const titleWordCount =
+								editor.plugins.wordcount.body.getCharacterCountWithoutSpaces();
+							titleWordCount <= wordCountLimit
+								? setData({ ...data, title: value })
+								: setData({ ...data });
+						}}
+						value={data.title}
+						init={{
+							skin: darkTheme ? "oxide-dark" : "oxide",
+							placeholder: "The post title...",
+							menubar: false,
+							toolbar: false,
+							inline: true,
+							plugins: "wordcount",
+							paste_as_text: true,
+						}}
+					/>
+					<div className={style.imageWrap}>
+						<div
+							className={`${style.mainImage} 
+							${data.mainImage === "" ? style.hide : ""}
+							`}
+						>
+							<button
+								onClick={() => {
+									mainImageRef.current.execCommand(
+										"mceImage"
+									);
+								}}
+							>
+								<div className={style.buttonText}>
+									<span>Set Main Image Source</span>
+									<span>{"( only jpeg, png, webp )"}</span>
+								</div>
+							</button>
+							<Editor
+								apiKey="x2zlv8pvui3hofp395wp6my8308b15h3s176scf930dizek1"
+								id="editorImage"
+								onInit={(_evt, editor) => {
+									setLoadCount(loadCount => loadCount + 1);
+									mainImageRef.current = editor;
+								}}
+								onEditorChange={value => {
+									setData({
+										...data,
+										mainImage: value,
+									});
+								}}
+								value={data.mainImage}
+								onNodeChange={evt => {
+									const target = evt.element;
+
+									const handleImage = url => {
+										const image = new Image();
+
+										image.onerror = () => {
+											target.remove();
+											setData({
+												...data,
+												mainImage: "",
+											});
+										};
+
+										image.src = url;
+									};
+
+									target.nodeName === "IMG" &&
+										handleImage(target.src);
+								}}
+								init={{
+									inline: true,
+									menubar: false,
+									toolbar: false,
+									plugins: ["image", "quickbars"],
+									image_uploadtab: false,
+									image_dimensions: false,
+									image_description: false,
+									object_resizing: false,
+									typeahead_urls: false,
+									quickbars_selection_toolbar: false,
+									quickbars_insert_toolbar: false,
+									quickbars_image_toolbar: "image",
+								}}
+								onKeyDown={() => false}
+								onPaste={() => false}
+							/>
+						</div>
+					</div>
+					<Editor
+						id="editorContent"
+						apiKey="x2zlv8pvui3hofp395wp6my8308b15h3s176scf930dizek1"
+						onInit={(_evt, editor) => {
+							setLoadCount(loadCount => loadCount + 1);
+							contentRef.current = editor;
+						}}
+						onEditorChange={(value, editor) => {
+							const wordCountLimit = 8000;
+							const newLineCount =
+								value
+									?.match(/(?<=>)[^<>\n]+(?=<)/g)
+									?.join(" ")
+									?.replace(/\s/g, "")
+									?.match(/(?<=)&nbsp;(?=)/g) ?? [];
+							const contentWordCount =
+								editor.plugins.wordcount.body.getCharacterCountWithoutSpaces() +
+								newLineCount.length;
+
+							contentWordCount <= wordCountLimit
+								? setData({ ...data, content: value })
+								: setData({ ...data });
+						}}
+						onNodeChange={evt => {
+							const target = evt.element;
+
+							const handleImage = url => {
+								const image = new Image();
+
+								image.onerror = () => {
+									target.remove();
+									setData({
+										...data,
+										content:
+											contentRef.current.getContent(),
+									});
+								};
+
+								image.src = url;
+							};
+
+							target.nodeName === "IMG" &&
+								handleImage(target.src);
+						}}
+						value={data.content}
+						init={{
+							placeholder: "The post content...",
+							inline: true,
+							menubar: false,
+							toolbar: false,
+							plugins: [
+								"quickbars",
+								"image",
+								"codesample",
+								"link",
+								"lists",
+								"wordcount",
+							],
+							...editorContentInit.selection_toolbar,
+							...editorContentInit.insert_toolbar,
+							...editorContentInit.image_toolbar,
+							paste_as_text: true,
+						}}
+					/>
+				</div>
+				{loadCount < 3 && <Loading />}
+			</div>
+		</div>
+	);
 };
 
 export default PostCreate;
