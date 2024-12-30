3c41665bde4bf882dc03b31028840f27ded9ae6c
Weiss Bai <whitesgr03@gmail.com>
2024-12-30 20:04:58 +0800

    feat!: delete UserProvider and Callback components

--- src/router.jsx
+++ src/router.jsx
@@ -2,49 +2,37 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './styles/index.css';
 
-import UserProvider from './components/UserProvider';
 import { Error } from './components/utils/Error/Error';
 import { NotFound } from './components/utils/Error/NotFound';
 import { Dashboard } from './components/pages/Dashboard/Dashboard';
 import { App } from './components/pages/App/App';
 import { PostCreate } from './components/pages/Post/PostCreate';
-import Callback from './components/Callback';
 
 export const Router = () => (
 	<RouterProvider
 		router={createBrowserRouter([
 			{
 				path: '/',
-				element: <UserProvider />,
+				element: <App />,
 				children: [
 					{
-						path: '/',
-						element: <App />,
-						children: [
-							{
-								path: '*',
-								element: <NotFound />,
-							},
-							{
-								index: true,
-								element: <Dashboard />,
-							},
+						path: '*',
+						element: <NotFound />,
+					},
+					{
+						index: true,
+						element: <Dashboard />,
+					},
 
-							{
-								path: 'post/editor',
-								element: <PostCreate />,
-							},
-						],
-					},
 					{
-						path: '/callback',
-						element: <Callback />,
-					},
-					{
-						path: 'error',
-						element: <Error />,
+						path: 'post/editor',
+						element: <PostCreate />,
 					},
 				],
+			},
+			{
+				path: 'error',
+				element: <Error />,
 			},
 		])}
 	/>
