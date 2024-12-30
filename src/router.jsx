54e861ade54bc049720efb207e3b4d8aa63a6a3c
Weiss Bai <whitesgr03@gmail.com>
2024-12-30 20:02:44 +0800

    style: change double quote to single quote

--- src/router.jsx
+++ src/router.jsx
@@ -1,28 +1,28 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import "./styles/index.css";
+import './styles/index.css';
 
-import UserProvider from "./components/UserProvider";
-import { Error } from "./components/utils/Error/Error";
-import { NotFound } from "./components/utils/Error/NotFound";
-import { Dashboard } from "./components/pages/Dashboard/Dashboard";
-import { App } from "./components/pages/App/App";
-import { PostCreate } from "./components/pages/Post/PostCreate";
-import Callback from "./components/Callback";
+import UserProvider from './components/UserProvider';
+import { Error } from './components/utils/Error/Error';
+import { NotFound } from './components/utils/Error/NotFound';
+import { Dashboard } from './components/pages/Dashboard/Dashboard';
+import { App } from './components/pages/App/App';
+import { PostCreate } from './components/pages/Post/PostCreate';
+import Callback from './components/Callback';
 
 export const Router = () => (
 	<RouterProvider
 		router={createBrowserRouter([
 			{
-				path: "/",
+				path: '/',
 				element: <UserProvider />,
 				children: [
 					{
-						path: "/",
+						path: '/',
 						element: <App />,
 						children: [
 							{
-								path: "*",
+								path: '*',
 								element: <NotFound />,
 							},
 							{
@@ -31,17 +31,17 @@ export const Router = () => (
 							},
 
 							{
-								path: "post/editor",
+								path: 'post/editor',
 								element: <PostCreate />,
 							},
 						],
 					},
 					{
-						path: "/callback",
+						path: '/callback',
 						element: <Callback />,
 					},
 					{
-						path: "error",
+						path: 'error',
 						element: <Error />,
 					},
 				],
