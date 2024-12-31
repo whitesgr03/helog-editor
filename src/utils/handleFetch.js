e1de738c6839eef2f1fd7febb6b87e22df991f5d
Weiss Bai <whitesgr03@gmail.com>
2024-12-31 14:05:09 +0800

    feat!: if the status code is not 200, handleFetch will return the status code

--- src/utils/handleFetch.js
+++ src/utils/handleFetch.js
@@ -1,16 +1,15 @@
-const handleFetch = async (url, option) => {
-	let result = null;
+export const handleFetch = async (url, option) => {
 	try {
 		const response = await fetch(url, { ...option });
-		result = await response.json();
+		const result = await response.json();
+		response.status !== 200 && (result.status = response.status);
+		return result;
 	} catch (err) {
-		!option.signal.aborted &&
-			(result = {
+		return (
+			!option.signal.aborted && {
 				success: false,
 				message: err,
-			});
+			}
+		);
 	}
-	return result;
 };
-
-export default handleFetch;
