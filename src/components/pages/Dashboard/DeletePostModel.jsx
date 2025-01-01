ffb3a14af1a8c290ecfb9cb8333a0485b2e18d67
Weiss Bai <whitesgr03@gmail.com>
2025-01-01 09:39:13 +0800

    feat: create handle delete post function

--- src/components/pages/Dashboard/DeletePostModel.jsx
+++ src/components/pages/Dashboard/DeletePostModel.jsx
@@ -1,11 +1,42 @@
 // Packages
+import { useState } from 'react';
+import { useOutletContext, useNavigate, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // Styles
 import styles from './DeletePostModel.module.css';
 import buttonStyles from '../../../styles/button.module.css';
 
-export const DeletePostModel = ({ onDelete, title }) => {
+import { deletePost } from '../../../utils/handlePost';
+
+export const DeletePostModel = ({ id, title }) => {
+	const { onActiveModal, onAlert, onPosts } = useOutletContext();
+	const [loading, setLoading] = useState(false);
+
+	const { pathname: previousPath } = useLocation();
+	const navigate = useNavigate();
+
+	const handleDeletePost = async () => {
+		setLoading(true);
+
+		const result = await deletePost({
+			postId: id,
+		});
+
+		const handleSuccess = () => {
+			onPosts(result.data);
+			onAlert({ message: `Deleted ${title} successfully`, error: false });
+			onActiveModal({ component: null });
+		};
+
+		result.success
+			? await handleSuccess()
+			: navigate('/dashboard/error', {
+					state: { error: result.message, previousPath },
+				});
+
+		setLoading(false);
+	};
 	return (
 		<div className={styles['delete-model']}>
 			<span className={styles.title}>Delete Post</span>
@@ -17,7 +48,10 @@ export const DeletePostModel = ({ onDelete, title }) => {
 				<button className={buttonStyles.cancel} data-close-model>
 					Cancel
 				</button>
-				<button className={buttonStyles.error} onClick={onDelete}>
+				<button
+					className={buttonStyles.error}
+					onClick={() => !loading && handleDeletePost()}
+				>
 					Delete
 				</button>
 			</div>
