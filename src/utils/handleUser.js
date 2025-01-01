95c43138d298c2ab814940efcbee7abd44099e81
Weiss Bai <whitesgr03@gmail.com>
2025-01-01 10:28:28 +0800

    feat!: delete updateUser and deleteUser

--- src/utils/handleUser.js
+++ src/utils/handleUser.js
@@ -12,25 +12,3 @@ export const getUser = async token => {
 
 	return await handleFetch(url, options);
 };
-
-export const updateUser = async (token, fields) => {
-	const options = {
-		method: 'PUT',
-		headers: {
-			Authorization: `Bearer ${token}`,
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(fields),
-	};
-	return await handleFetch(url, options);
-};
-
-export const deleteUser = async token => {
-	const options = {
-		method: 'DELETE',
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
-	return await handleFetch(url, options);
-};
