82c891a5bd4464b4ad3c2422c8c60aae293c693b
Weiss Bai <whitesgr03@gmail.com>
2025-01-01 10:43:17 +0800

    feat: create updateUser function

--- src/utils/handleUser.js
+++ src/utils/handleUser.js
@@ -11,3 +11,15 @@ export const getUser = async ({ signal }) => {
 
 	return await handleFetch(url, options);
 };
+
+export const updateUser = async fields => {
+	const options = {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		credentials: 'include',
+		body: JSON.stringify(fields),
+	};
+	return await handleFetch(url, options);
+};
