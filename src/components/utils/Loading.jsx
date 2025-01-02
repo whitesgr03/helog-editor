ddab8c4bf86a3d8283cd8a36a7adad545b5fe520
Weiss Bai <whitesgr03@gmail.com>
2025-01-02 12:51:33 +0800

    feat!: update Loading component and module css

--- src/components/utils/Loading.jsx
+++ src/components/utils/Loading.jsx
@@ -1,12 +1,24 @@
+// Packages
+import PropTypes from 'prop-types';
+
 // Styles
 import styles from './Loading.module.css';
 import imageStyles from '../../styles/image.module.css';
 
-export const Loading = () => {
+export const Loading = ({ text, dark, light, shadow }) => {
 	return (
-		<div className={styles.loading}>
+		<div
+			className={`${styles.loading} ${dark ? styles.dark : ''} ${light ? styles.light : ''} ${shadow ? styles.shadow : ''}`}
+		>
 			<span className={`${imageStyles.icon} ${styles.load}`} />
-			Loading ...
+			{text}
 		</div>
 	);
+};
+
+Loading.propTypes = {
+	text: PropTypes.string,
+	dark: PropTypes.bool,
+	light: PropTypes.bool,
+	shadow: PropTypes.bool,
 };
