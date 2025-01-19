7da85b7d376972547ab76303ad968bf0aab28c48
Weiss Bai <whitesgr03@gmail.com>
2025-01-19 18:12:40 +0800

    feat: changed the value of the key message of object parameter pass to onAlert

--- src/components/pages/Dashboard/DeletePostModel.jsx
+++ src/components/pages/Dashboard/DeletePostModel.jsx
@@ -34,7 +34,7 @@ export const DeletePostModel = ({
 
 		const handleSuccess = () => {
 			onDeletePost(id);
-			onAlert({ message: `Deleted ${title} successfully`, error: false });
+      onAlert({ message: `Post ${title} has been Deleted`, error: false });
 			onActiveModal({ component: null });
 		};
 
