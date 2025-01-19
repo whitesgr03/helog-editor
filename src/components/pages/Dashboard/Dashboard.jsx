65ef797c14916a29946435b1991c215a387c7be6
Weiss Bai <whitesgr03@gmail.com>
2025-01-19 14:47:39 +0800

    feat!: removed conditions for rendering Navigate or Loading components

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 // Package
 import { useEffect, useState } from 'react';
-import { Link, Navigate, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // Styles
 import styles from './Dashboard.module.css';
@@ -8,15 +8,10 @@ import buttonStyles from '../../../styles/button.module.css';
 
 // Component
 import { TableRows } from './TableRows';
-import { Loading } from '../../utils/Loading';
 
 export const Dashboard = () => {
 	const [posts, setPosts] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
 	const [publishing, setPublishing] = useState(false);
-
-	const { pathname: previousPath } = useLocation();
 
 	const handleUpdatePost = newPost => {
 		setPosts(posts.map(post => (post.id === newPost.id ? newPost : post)));
@@ -27,51 +22,43 @@ export const Dashboard = () => {
 	};
 
 	return (
-		<>
-			{error ? (
-				<Navigate to="/error" state={{ error, previousPath }} />
-			) : loading ? (
-				<Loading text={'Loading...'} />
-			) : (
-				<div className={styles.dashboard}>
-					<h2>Dashboard</h2>
-					<div className={styles['button-wrap']}>
-						<span>{posts.length > 0 && `Total posts: ${posts.length}`}</span>
-						<Link to="/post/editor" className={buttonStyles.success}>
-							New Post
-						</Link>
-					</div>
-					<div className={styles.container}>
-						{posts.length > 0 ? (
-							<table>
-								<thead>
-									<tr>
-										<th>Title</th>
-										<th>Publish</th>
-										<th>Last Modified</th>
-										<th>Edit</th>
-										<th>Delete</th>
-									</tr>
-								</thead>
-								<tbody>
-									{posts.map(post => (
-										<TableRows
-											key={post.id}
-											post={post}
-											publishing={publishing}
-											onPublishing={setPublishing}
-											onUpdatePost={handleUpdatePost}
-											onDeletePost={handleDeletePost}
-										/>
-									))}
-								</tbody>
-							</table>
-						) : (
-							<p>There are not posts.</p>
-						)}
-					</div>
-				</div>
-			)}
-		</>
+		<div className={styles.dashboard}>
+			<h2>Dashboard</h2>
+			<div className={styles['button-wrap']}>
+				<span>{posts.length > 0 && `Total posts: ${posts.length}`}</span>
+				<Link to="/post/editor" className={buttonStyles.success}>
+					New Post
+				</Link>
+			</div>
+			<div className={styles.container}>
+				{posts.length > 0 ? (
+					<table>
+						<thead>
+							<tr>
+								<th>Title</th>
+								<th>Publish</th>
+								<th>Last Modified</th>
+								<th>Edit</th>
+								<th>Delete</th>
+							</tr>
+						</thead>
+						<tbody>
+							{posts.map(post => (
+								<TableRows
+									key={post.id}
+									post={post}
+									publishing={publishing}
+									onPublishing={setPublishing}
+									onUpdatePost={handleUpdatePost}
+									onDeletePost={handleDeletePost}
+								/>
+							))}
+						</tbody>
+					</table>
+				) : (
+					<p>There are not posts.</p>
+				)}
+			</div>
+		</div>
 	);
 };
