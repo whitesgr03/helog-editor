92cdbe5f5abb5a83309754130fa4662dd999d9bf
Weiss Bai <whitesgr03@gmail.com>
2025-01-23 15:22:38 +0800

    feat!: removed getPosts and getPost functions

--- src/utils/handlePost.js
+++ src/utils/handlePost.js
@@ -2,25 +2,6 @@ import { handleFetch } from './handleFetch';
 
 const url = `${import.meta.env.VITE_RESOURCE_URL}/blog/posts`;
 
-export const getPosts = async ({ signal }) => {
-	const options = {
-		method: 'GET',
-		signal,
-		credentials: 'include',
-	};
-	return await handleFetch(url, options);
-};
-
-export const getPost = async ({ postId }) => {
-	const options = {
-		method: 'GET',
-		credentials: 'include',
-	};
-	const result = await handleFetch(`${url}/${postId}`, options);
-
-	return result;
-};
-
 export const createPost = async ({ data }) => {
 	const options = {
 		method: 'POST',
