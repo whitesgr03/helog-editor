cbe0ff73ea766bfea373fa0033f268c48b315b0b
Weiss Bai <whitesgr03@gmail.com>
2025-01-28 18:44:06 +0800

    feat: created PostEditor component and module css
    The PostEditor component is displayed when a user needs to edit any post he has created.
    
    When the post content is changed, the component will be autosaved .

---
+++ src/components/pages/Post/PostEditor.jsx
@@ -0,0 +1,562 @@
+// Packages
+import { useState, useRef, useEffect } from 'react';
+import {
+	useOutletContext,
+	useNavigate,
+	Link,
+	useParams,
+} from 'react-router-dom';
+import { Editor } from '@tinymce/tinymce-react';
+import { string } from 'yup';
+import isEmpty from 'lodash.isempty';
+import isEqual from 'lodash.isequal';
+
+// Styles
+import buttonStyles from '../../../styles/button.module.css';
+import styles from './PostEditor.module.css';
+import imageStyles from '../../../styles/image.module.css';
+import formStyles from '../../../styles/form.module.css';
+
+// Utils
+import { createPost, updatePost } from '../../../utils/handlePost';
+import { verifySchema } from '../../../utils/verifySchema';
+
+// Components
+import { Loading } from '../../utils/Loading';
+import { PossMainImageUpdate } from './PossMainImageUpdate';
+
+// Variables
+const EDITOR_TITLE_INIT = {
+	entity_encoding: 'raw',
+	placeholder: 'The post title...',
+	menubar: false,
+	toolbar: false,
+	inline: true,
+	plugins: 'wordcount',
+	paste_as_text: true,
+};
+const EDITOR_CONTENT_INIT = {
+	placeholder: 'The post content...',
+	inline: true,
+	menubar: false,
+	toolbar: false,
+	plugins: ['quickbars', 'image', 'codesample', 'link', 'lists', 'wordcount'],
+	paste_as_text: true,
+
+	quickbars_selection_toolbar: 'bold italic link blocks blockquote',
+	link_default_target: '_blank',
+	link_assume_external_targets: 'https',
+	link_context_toolbar: true,
+	link_title: false,
+	block_formats: 'Large=h3; Normal=p; Small=h5',
+
+	quickbars_insert_toolbar: 'bullist numlist image codesample hr',
+	quickbars_image_toolbar: 'image',
+	image_uploadtab: false,
+	typeahead_urls: false,
+};
+
+export const PostEditor = () => {
+	const {
+		posts,
+		darkTheme,
+		onAlert,
+		onCreatePost,
+		onUpdatePost,
+		onActiveModal,
+	} = useOutletContext();
+
+	const { postId } = useParams();
+
+	const post = posts.find(post => post._id === postId);
+
+	const default_editor_fields = {
+		title: post?.title ?? '',
+		mainImage: post?.mainImage ?? '',
+		content: post?.content ?? '',
+		publish: post?.publish ?? false,
+	};
+
+	const [editorFields, setEditorFields] = useState(default_editor_fields);
+	const [previousEditorFields, setPreviousEditorFields] = useState(
+		default_editor_fields,
+	);
+
+	const [fieldsErrors, setFieldsErrors] = useState({});
+	const [previewImage, setPreviewImage] = useState(false);
+	const [publishing, setPublishing] = useState(false);
+	const [saving, setSaving] = useState(false);
+
+	const [titleEditorLoad, setTitleEditorLoad] = useState(false);
+	const [contentEditorLoad, setContentEditorLoad] = useState(false);
+	const [titleLength, setTitleLength] = useState(-1);
+	const [contentLength, setContentLength] = useState(-1);
+	const [title, setTitle] = useState(null);
+	const [content, setContent] = useState(null);
+
+	const imageWrapRef = useRef(null);
+	const previewImageWrapRef = useRef(null);
+	const timer = useRef(null);
+	const titleRef = useRef(null);
+	const contentRef = useRef(null);
+
+	const navigate = useNavigate();
+
+	const handleShowPreview = () => {
+		const imageWrapHeight = imageWrapRef.current.clientHeight;
+
+		!previewImage
+			? (previewImageWrapRef.current.style.maxHeight = `${imageWrapHeight}px`)
+			: (previewImageWrapRef.current.style.maxHeight = '');
+
+		setPreviewImage(!previewImage);
+	};
+
+	const handlePublish = async () => {
+		setPublishing(true);
+
+		const result = await updatePost({
+			postId,
+			data: { ...editorFields, publish: !editorFields.publish },
+		});
+
+		const handleSuccess = () => {
+			setEditorFields({ ...editorFields, publish: !editorFields.publish });
+			onAlert({
+				message: `Post has been ${editorFields.publish ? 'Published' : 'UnPublish'}.`,
+			});
+			onUpdatePost(result.data);
+		};
+
+		result.success
+			? handleSuccess()
+			: result.fields
+				? setFieldsErrors({ ...result.fields })
+				: onAlert({
+						message: 'There are some errors occur, please try again later.',
+						error: true,
+						delay: 3000,
+					});
+
+		setPublishing(false);
+	};
+
+	const handleCreate = async fields => {
+		const result = await createPost({ data: fields });
+
+		const handleSuccess = () => {
+			onAlert({ message: 'Autosaving...' });
+			setPreviousEditorFields(fields);
+			onCreatePost(result.data);
+			navigate(`${result.data._id}`);
+		};
+
+		result.success
+			? handleSuccess()
+			: result.fields
+				? setFieldsErrors({ ...result.fields })
+				: onAlert({
+						message: 'There are some errors occur, please try again later.',
+						error: true,
+						delay: 3000,
+					});
+	};
+
+	const handleUpdate = async fields => {
+		const result = await updatePost({ postId, data: fields });
+
+		const handleSuccess = () => {
+			onAlert({ message: 'Autosaving...' });
+			setPreviousEditorFields(fields);
+			onUpdatePost(result.data);
+			setFieldsErrors({});
+		};
+
+		result.success
+			? handleSuccess()
+			: result.fields
+				? setFieldsErrors({ ...result.fields })
+				: onAlert({
+						message: 'There are some errors occur, please try again later.',
+						error: true,
+						delay: 3000,
+					});
+	};
+
+	const handleSetMainImage = url => {
+		timer.current && clearTimeout(timer.current);
+
+		const newFields = { ...editorFields, mainImage: url };
+		const { mainImage, ...rest } = fieldsErrors;
+
+		setFieldsErrors({ ...rest });
+		setEditorFields(newFields);
+
+		timer.current = setTimeout(async () => {
+			postId ? await handleUpdate(newFields) : await handleCreate(newFields);
+		}, 2000);
+	};
+
+	const handleChangeTitle = (value, editor) => {
+		timer.current && clearTimeout(timer.current);
+
+		const limit = 100;
+		const wordCount = editor.getContent().length;
+
+		const { title, ...errors } = fieldsErrors;
+
+		setFieldsErrors({ ...errors });
+		setTitleLength(wordCount);
+
+		const handleValidation = async () => {
+			const newFields = { ...editorFields, title: value };
+
+			const schema = {
+				title: string()
+					.trim()
+					.when('$publish', ([publish], schema) =>
+						publish ? schema.required('Title is required.') : schema,
+					),
+			};
+
+			const validationResult = await verifySchema({
+				schema,
+				data: newFields,
+				context: { publish: editorFields.publish },
+			});
+
+			const handleInValid = () => {
+				setFieldsErrors({ ...errors, ...validationResult.fields });
+			};
+
+			const handleValid = async () => {
+				setEditorFields(newFields);
+				timer.current = setTimeout(async () => {
+					postId
+						? await handleUpdate(newFields)
+						: await handleCreate(newFields);
+				}, 2000);
+			};
+
+			validationResult.success ? handleValid() : handleInValid();
+		};
+
+		previousEditorFields.title !== value
+			? wordCount > limit
+				? setFieldsErrors({
+						...errors,
+						title: `Title must be less than ${limit} long.`,
+					})
+				: handleValidation()
+			: setEditorFields(previousEditorFields);
+	};
+
+	const handleChangeContent = (value, editor) => {
+		timer.current && clearTimeout(timer.current);
+
+		const limit = 8000;
+		const wordCount = editor.getContent({ format: 'text' }).length;
+		const { content, ...errors } = fieldsErrors;
+
+		setFieldsErrors({ ...errors });
+		setContentLength(wordCount);
+
+		const handleValidation = async () => {
+			const newFields = { ...editorFields, content: value };
+
+			const schema = {
+				content: string()
+					.trim()
+					.when('$publish', ([publish], schema) =>
+						publish ? schema.required('Content is required.') : schema,
+					),
+			};
+
+			const validationResult = await verifySchema({
+				schema,
+				data: newFields,
+				context: { publish: editorFields.publish },
+			});
+
+			const handleInValid = () => {
+				setFieldsErrors({ ...errors, ...validationResult.fields });
+			};
+
+			const handleValid = () => {
+				setEditorFields(newFields);
+				timer.current = setTimeout(async () => {
+					postId
+						? await handleUpdate(newFields)
+						: await handleCreate(newFields);
+				}, 2000);
+			};
+
+			validationResult.success ? handleValid() : handleInValid();
+		};
+
+		previousEditorFields.content !== value
+			? wordCount > limit
+				? setFieldsErrors({
+						...errors,
+						content: `Content must be less than ${limit} long.`,
+					})
+				: handleValidation()
+			: setEditorFields(previousEditorFields);
+	};
+
+	const handleResizeEditor = evt => {
+		evt.target.setAttribute(
+			'style',
+			`width:${evt.width}px;height:${evt.height}px;`,
+		);
+	};
+
+	const handleContentImages = (evt, editor) => {
+		const target = evt.element;
+		const value = editor.getContent();
+		const handleCheckMimeTypes = () => {
+			const isSetStyle = target.hasAttribute('style');
+			const width = target.getAttribute('width');
+			const height = target.getAttribute('height');
+
+			!isSetStyle &&
+				target.setAttribute('style', `width:${width}px;height:${height}px;`);
+
+			const image = new Image();
+			const handleError = () => {
+				onAlert({ message: 'URL is not a valid image source.' });
+				target.remove();
+				document.activeElement.blur();
+			};
+			image.onerror = handleError;
+
+			image.onload = () =>
+				(image.width <= 0 || image.height <= 0) && handleError();
+
+			image.src = target.src;
+		};
+		target.nodeName === 'IMG' &&
+			value !== editorFields.content &&
+			handleCheckMimeTypes();
+	};
+
+	const handleSaving = async () => {
+		setSaving(true);
+
+		timer.current && clearTimeout(timer.current);
+
+		postId
+			? await handleUpdate(editorFields)
+			: await handleCreate(editorFields);
+
+		setSaving(false);
+	};
+
+	useEffect(() => {
+		const handleSetContent = () => {
+			setTitle(post?.title ?? '');
+			setContent(post?.content ?? '');
+
+			setTitleLength(titleRef.current.getContent().length);
+			setContentLength(contentRef.current.getContent().length);
+		};
+		title === null &&
+			content === null &&
+			titleEditorLoad &&
+			contentEditorLoad &&
+			handleSetContent();
+		return () => clearTimeout(timer.current);
+	}, [titleEditorLoad, contentEditorLoad, title, content, post]);
+
+	useEffect(() => {
+		const handleFocusTitle = () => {
+			titleRef.current.selection.select(titleRef.current.getBody(), true);
+			titleRef.current.selection.collapse(false);
+		};
+		title !== null &&
+			titleEditorLoad &&
+			contentEditorLoad &&
+			handleFocusTitle();
+	}, [titleEditorLoad, contentEditorLoad, title]);
+
+	return (
+		<div className={styles.editor}>
+			{(!titleEditorLoad || !contentEditorLoad) && (
+				<Loading text={'Loading...'} />
+			)}
+			<div
+				className={`${styles.container} ${!titleEditorLoad || !contentEditorLoad ? styles.loading : ''}`}
+			>
+				<div className={styles['button-container']}>
+					<Link to="/" className={styles.link}>
+						<span className={`${styles['left-arrow']} ${imageStyles.icon}`} />
+						Back to Dashboard
+					</Link>
+
+					<div className={styles['button-wrap']}>
+						{!isEqual(previousEditorFields, editorFields) && (
+							<button
+								className={`${buttonStyles.content} ${buttonStyles.highlight}`}
+								onClick={() =>
+									!saving && isEmpty(fieldsErrors) && handleSaving()
+								}
+							>
+								{saving ? (
+									<span className={buttonStyles.text}>
+										Saving
+										<span
+											className={`${imageStyles.icon} ${buttonStyles['load']}`}
+										/>
+									</span>
+								) : (
+									<span>Save Post</span>
+								)}
+							</button>
+						)}
+
+						{postId && (
+							<button
+								className={`${buttonStyles.content} ${editorFields.publish ? buttonStyles.error : buttonStyles.success}`}
+								onClick={handlePublish}
+							>
+								{publishing ? (
+									<span className={buttonStyles.text}>
+										{editorFields.publish ? 'Unpublishing' : 'Publishing'}
+										<span
+											className={`${imageStyles.icon} ${buttonStyles['load']}`}
+										/>
+									</span>
+								) : (
+									<span>
+										{editorFields.publish ? 'Unpublished' : 'Published'} Post
+									</span>
+								)}
+							</button>
+						)}
+					</div>
+				</div>
+
+				<div className={styles.editors}>
+					<div>
+						<Editor
+							id="editor-title"
+							key={darkTheme}
+							tinymceScriptSrc="/tinymce/tinymce.min.js"
+							initialValue={title ?? ''}
+							licenseKey="gpl"
+							tagName="h2"
+							onInit={(_evt, editor) => {
+								setTitleEditorLoad(true);
+								titleRef.current = editor;
+							}}
+							onEditorChange={handleChangeTitle}
+							init={{
+								...EDITOR_TITLE_INIT,
+								skin: darkTheme ? 'oxide-dark' : 'oxide',
+							}}
+						/>
+						<div className={styles['error-message-wrap']}>
+							<div
+								className={`${formStyles['error-message']} ${fieldsErrors.title ? formStyles['error-message-show'] : formStyles['error-message-no-display']}`}
+							>
+								<span className={`${imageStyles.icon} ${formStyles.alert}`} />
+								<span className={formStyles.placeholder}>
+									{fieldsErrors.title ?? 'Message placeholder'}
+								</span>
+							</div>
+
+							{titleLength > -1 && (
+								<div className={styles.count}>
+									Title word count: {titleLength}
+								</div>
+							)}
+						</div>
+					</div>
+					<div className={styles['preview-wrap']}>
+						<div className={styles['preview']}>
+							<button
+								className={styles['preview-title']}
+								onClick={handleShowPreview}
+							>
+								Post main image
+								<span
+									className={`${styles['right-arrow']} ${previewImage ? styles['right-arrow-turn-down'] : ''} ${imageStyles.icon}`}
+								/>
+							</button>
+							<div
+								className={styles['preview-image-wrap']}
+								ref={previewImageWrapRef}
+							>
+								<button
+									className={styles['preview-image']}
+									ref={imageWrapRef}
+									onClick={() =>
+										onActiveModal({
+											component: (
+												<PossMainImageUpdate
+													onActiveModal={onActiveModal}
+													onSetMainImage={handleSetMainImage}
+												/>
+											),
+										})
+									}
+								>
+									{editorFields.mainImage === '' ? (
+										<div className={styles['image-wrap']}>
+											<span>Click to set image url</span>
+										</div>
+									) : (
+										<img
+											className={styles.image}
+											src={editorFields.mainImage}
+											alt="Main Image Preview"
+										/>
+									)}
+								</button>
+							</div>
+						</div>
+						<div
+							className={`${formStyles['error-message']} ${fieldsErrors.mainImage ? formStyles['error-message-show'] : ''}`}
+						>
+							<span className={`${imageStyles.icon} ${formStyles.alert}`} />
+							<span className={formStyles.placeholder}>
+								{fieldsErrors.mainImage ?? 'Message placeholder'}
+							</span>
+						</div>
+					</div>
+					<div>
+						<Editor
+							id="editor-content"
+							tinymceScriptSrc="/tinymce/tinymce.min.js"
+							licenseKey="gpl"
+							initialValue={content ?? ''}
+							onInit={(_evt, editor) => {
+								setContentEditorLoad(true);
+								contentRef.current = editor;
+							}}
+							onEditorChange={handleChangeContent}
+							onObjectResized={handleResizeEditor}
+							onNodeChange={handleContentImages}
+							init={EDITOR_CONTENT_INIT}
+						/>
+						<div className={styles['error-message-wrap']}>
+							<div
+								className={`${formStyles['error-message']} ${fieldsErrors.content ? formStyles['error-message-show'] : formStyles['error-message-no-display']}`}
+							>
+								<span className={`${imageStyles.icon} ${formStyles.alert}`} />
+								<span className={formStyles.placeholder}>
+									{fieldsErrors.content ?? 'Message placeholder'}
+								</span>
+							</div>
+							{contentLength > -1 && (
+								<div className={styles.count}>
+									Content word count: {contentLength}
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
