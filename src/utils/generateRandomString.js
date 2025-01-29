7cb95ec2aed6bb95087675d77c357fad83b9a74b
Weiss Bai <whitesgr03@gmail.com>
2025-01-29 21:59:51 +0800

    docs: removed generateCodeChallenge and generateRandomString functions

--- src/utils/generateRandomString.js
+++
@@ -1,9 +0,0 @@
-const characters =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-const generateRandomString = (length = 32) =>
-	Array.from(window.crypto.getRandomValues(new Uint32Array(length)), value =>
-		characters.charAt(value % characters.length)
-	).join("");
-
-export default generateRandomString;
