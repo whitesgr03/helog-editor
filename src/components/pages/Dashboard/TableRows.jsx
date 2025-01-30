3fc4abce5ddfcf60cfe6886f70eb3d56ef90243a
Weiss Bai <whitesgr03@gmail.com>
2025-01-30 20:43:28 +0800

    feat: changed the "to" prop value from Link component

--- src/components/pages/Dashboard/TableRows.jsx
+++ src/components/pages/Dashboard/TableRows.jsx
@@ -74,7 +74,7 @@ export const TableRows = ({
 				<span>{format(post.updatedAt, 'MMMM d, y')}</span>
 			</td>
 			<td className={styles['button-wrap']}>
-				<Link to="/post/editor" className={styles.link}>
+				<Link to={`/editor/post/${post._id}`} className={styles.link}>
 					<span className={`${imageStyles.icon} ${styles.edit}`} />
 				</Link>
 			</td>
