2a28b802059430fc1a765fa85ef7d89f2198c032
Weiss Bai <whitesgr03@gmail.com>
2025-01-30 17:44:11 +0800

    feat: removed button-wrap class

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -17,7 +17,7 @@ export const Dashboard = () => {
 	return (
 		<div className={styles.dashboard}>
 			<h2>Dashboard</h2>
-			<div className={styles['button-wrap']}>
+			<div>
 				<span>{posts.length > 0 && `Total posts: ${posts.length}`}</span>
 				<Link to="/post/editor" className={buttonStyles.success}>
 					New Post
