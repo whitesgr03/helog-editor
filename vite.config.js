d831da7a3287fbd658b3c5c016b4f3f3e5acd1dc
Weiss Bai <whitesgr03@gmail.com>
2025-02-15 19:26:40 +0800

    feat: added test options

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
 	html: {
 		cspNonce: randomBytes(16).toString('base64'),
 	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/__test__/setup.js',
+		include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+		exclude: ['src/**/delete_*.{test,spec}.?(c|m)[jt]s?(x)'],
+		coverage: {
+			include: ['src/**'],
+			exclude: ['src/**/delete_**'],
+		},
+	},
 });
