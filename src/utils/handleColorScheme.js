36395420fa2dff0d548e100290682303f8ae26a2
Weiss Bai <whitesgr03@gmail.com>
2025-02-15 19:29:45 +0800

    feat!: removed the handleColorScheme file

--- src/utils/handleColorScheme.js
+++
@@ -1,9 +0,0 @@
-const handleColorScheme = () => {
-	const darkTheme =
-		window.matchMedia("(prefers-color-scheme: dark)")?.matches ?? false;
-
-	localStorage.setItem("heLog.darkTheme", JSON.stringify(darkTheme));
-
-	return darkTheme;
-};
-export default handleColorScheme;
