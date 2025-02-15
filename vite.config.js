5f21d85950f4a43ad2e448c7625df60c4c22a723
Weiss Bai <whitesgr03@gmail.com>
2025-02-15 19:26:25 +0800

    chore: add vite html csp nonce

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { randomBytes } from 'node:crypto';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	html: {
+		cspNonce: randomBytes(16).toString('base64'),
+	},
 });
