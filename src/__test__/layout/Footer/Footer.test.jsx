da54dd92e7a71490b7e42511bedf9888047f27bb
Weiss Bai <whitesgr03@gmail.com>
2025-02-15 19:40:09 +0800

    feat: created the footer component test and snapshot

---
+++ src/__test__/layout/Footer/Footer.test.jsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import { render } from '@testing-library/react';
+
+import { Footer } from '../../../components/layout/Footer/Footer';
+
+describe('Footer component', () => {
+	it('should match snapshot', () => {
+		const { asFragment } = render(<Footer />);
+
+		const actual = asFragment();
+
+		expect(actual).toMatchSnapshot();
+	});
+});
