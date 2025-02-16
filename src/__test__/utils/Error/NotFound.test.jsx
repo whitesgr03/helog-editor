1636f2be4c232e6de44ca9a8e4bde3e639ae38e2
Weiss Bai <whitesgr03@gmail.com>
2025-02-16 18:59:01 +0800

    feat: created the NotFound component tests and a snapshot

---
+++ src/__test__/utils/Error/NotFound.test.jsx
@@ -0,0 +1,37 @@
+import { expect, describe, it } from 'vitest';
+import { render } from '@testing-library/react';
+
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+
+import { NotFound } from '../../../components/utils/Error/NotFound';
+
+describe('NotFound component', () => {
+	it('should match snapshot', () => {
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <NotFound />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		const { asFragment } = render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const actual = asFragment();
+
+		expect(actual).toMatchSnapshot();
+	});
+});
