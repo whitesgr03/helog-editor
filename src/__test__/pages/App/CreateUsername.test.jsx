29be2b2652d2ca9df8d925797cdd04b51c71ca69
Weiss Bai <whitesgr03@gmail.com>
2025-02-18 19:04:52 +0800

    feat: created the CreateUsername component tests

---
+++ src/__test__/pages/App/CreateUsername.test.jsx
@@ -0,0 +1,300 @@
+import { vi, describe, it, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+
+import { CreateUsername } from '../../../components/pages/App/CreateUsername';
+import { Loading } from '../../../components/utils/Loading';
+
+import { updateUser } from '../../../utils/handleUser';
+
+vi.mock('../../../utils/handleUser');
+vi.mock('../../../components/utils/Loading');
+
+describe('CreateUsername component', () => {
+	it.only('should change a field values if the field is entered', async () => {
+		const user = userEvent.setup();
+		const mockProps = {};
+
+		const mockName = '_changed';
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <CreateUsername {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const usernameField = screen.getByLabelText('Create username', {
+			selector: 'input',
+		});
+
+		await user.type(usernameField, mockName);
+
+		expect(usernameField).toHaveValue(mockName);
+	});
+	it.only('should render an error field message if the field validation fails after submission', async () => {
+		const user = userEvent.setup();
+		const mockProps = {};
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <CreateUsername {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const submitButton = screen.getByRole('button', 'Save');
+		const usernameField = screen.getByText('Create username');
+		const usernameErrorMessage = screen.getByText('Message placeholder');
+
+		await user.click(submitButton);
+
+		expect(usernameField).toHaveClass(/error/);
+		expect(usernameErrorMessage).toHaveTextContent('Username is required.');
+	});
+	it.only('should validate each input after a failed submission', async () => {
+		const user = userEvent.setup();
+		const mockProps = {};
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <CreateUsername {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const submitButton = screen.getByRole('button', 'Save');
+		const usernameField = screen.getByText('Create username');
+		const usernameErrorMessage = screen.getByText('Message placeholder');
+
+		await user.click(submitButton);
+
+		expect(usernameField).toHaveClass(/error/);
+		expect(usernameErrorMessage).toHaveTextContent('Username is required.');
+
+		await user.type(usernameField, 'new_name');
+
+		await waitFor(() => {
+			expect(usernameField).not.toHaveClass(/error/);
+			expect(usernameErrorMessage).toHaveTextContent('Message placeholder');
+		});
+
+		await user.type(usernameField, '#&@*($#$');
+
+		await waitFor(() => {
+			expect(usernameField).toHaveClass(/error/);
+			expect(usernameErrorMessage).toHaveTextContent(
+				'Username must be alphanumeric.',
+			);
+		});
+	});
+	it.only('should render an error field message if the username update fails', async () => {
+		const user = userEvent.setup();
+		const mockProps = {};
+		const mockFetchResult = {
+			success: false,
+			fields: {
+				username: 'error',
+			},
+		};
+
+		updateUser.mockResolvedValueOnce(mockFetchResult);
+		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <CreateUsername {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const submitButton = screen.getByRole('button', 'Save');
+		const usernameLabel = screen.getByText('Create username');
+		const usernameField = screen.getByLabelText('Create username');
+		const usernameErrorMessage = screen.getByText('Message placeholder');
+
+		await user.type(usernameField, 'new_name');
+		user.click(submitButton);
+
+		const loadingComponent = await screen.findByText('Loading component');
+
+		expect(updateUser).toBeCalledTimes(1);
+		expect(usernameLabel).toHaveClass(/error/);
+		expect(usernameErrorMessage).toHaveTextContent(
+			mockFetchResult.fields.username,
+		);
+		expect(loadingComponent).not.toBeInTheDocument();
+	});
+	it.only('should render Error component if the username update fails', async () => {
+		const user = userEvent.setup();
+		const mockProps = {
+			onActiveModal: vi.fn(),
+			onError: vi.fn(),
+		};
+		const mockFetchResult = {
+			success: false,
+		};
+
+		updateUser.mockResolvedValueOnce(mockFetchResult);
+		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <CreateUsername {...mockProps} />,
+				},
+				{
+					path: '/error',
+					element: <div>Error page</div>,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const submitButton = screen.getByRole('button', 'Save');
+		const usernameField = screen.getByLabelText('Create username');
+
+		await user.type(usernameField, 'new_name');
+		user.click(submitButton);
+
+		const loadingComponent = await screen.findByText('Loading component');
+
+		expect(mockProps.onActiveModal).toBeCalledTimes(1);
+		expect(mockProps.onError).toBeCalledTimes(1);
+		expect(updateUser).toBeCalledTimes(1);
+		expect(loadingComponent).not.toBeInTheDocument();
+	});
+	it.only('should update the username if the username field successfully validates after user submission', async () => {
+		const user = userEvent.setup();
+		const mockProps = {
+			onUser: vi.fn(),
+			onActiveModal: vi.fn(),
+			onAlert: vi.fn(),
+		};
+		const mockFetchResult = {
+			success: true,
+			data: 'success',
+		};
+
+		updateUser.mockResolvedValueOnce(mockFetchResult);
+
+		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <CreateUsername {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const submitButton = screen.getByRole('button', 'Save');
+		const usernameField = screen.getByLabelText('Create username');
+
+		await user.type(usernameField, 'new_name');
+		user.click(submitButton);
+
+		const loadingComponent = await screen.findByText('Loading component');
+
+		expect(mockProps.onUser).toBeCalledWith(mockFetchResult.data);
+		expect(mockProps.onUser).toBeCalledTimes(1);
+		expect(mockProps.onActiveModal).toBeCalledTimes(1);
+		expect(mockProps.onAlert).toBeCalledTimes(1);
+		expect(updateUser).toBeCalledTimes(1);
+
+		expect(loadingComponent).not.toBeInTheDocument();
+	});
+});
