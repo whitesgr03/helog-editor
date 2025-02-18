d3eb2025c0a3a35dac3e1d1922d5af418269c24c
Weiss Bai <whitesgr03@gmail.com>
2025-02-18 09:00:19 +0800

    feat!: removed the navigate method in getColorTheme function

--- src/components/pages/App/App.jsx
+++ src/components/pages/App/App.jsx
@@ -84,7 +84,6 @@ export const App = () => {
 
 			localStorage.setItem('darkTheme', theme);
 			setDarkTheme(theme === 'true');
-			navigate('/');
 		};
 		darkTheme === null && getColorTheme();
 	}, [navigate, darkTheme, searchParams]);
