58d5d9870030392f3e34b9408aed188b7867197c
Weiss Bai <whitesgr03@gmail.com>
2025-02-18 11:04:42 +0800

    feat: changed the HeLog anchor element to Link component

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,6 +1,7 @@
 // Packages
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 // Styles
 import styles from './Header.module.css';
@@ -17,56 +18,51 @@ export const Header = ({ user, darkTheme, onColorTheme }) => {
 	const handleCloseDropdown = () => setActiveDropdown(false);
 
 	return (
-		<>
-			<header className={styles.header}>
-				<a
-					className={styles.logo}
-					href={`${import.meta.env.VITE_HELOG_URL}?theme=${darkTheme}`}
-				>
-					<h1>HeLog</h1>
-				</a>
-				<nav>
-					<ul className={styles.list}>
-						<li className={styles['toggle-btn']}>
-							<button onClick={onColorTheme}>
-								<div className={buttonStyles.theme}>
-									<span
-										data-testid="theme-icon"
-										className={`${imageStyles.icon} ${
-											darkTheme ? styles.moon : styles.sun
-										}`}
-									/>
-									<div>
-										<div />
-									</div>
+		<header className={styles.header}>
+			<Link to="/" className={styles.logo}>
+				<h1>HeLog Editor</h1>
+			</Link>
+			<nav>
+				<ul className={styles.list}>
+					<li className={styles['toggle-btn']}>
+						<button onClick={onColorTheme}>
+							<div className={buttonStyles.theme}>
+								<span
+									data-testid="theme-icon"
+									className={`${imageStyles.icon} ${
+										darkTheme ? styles.moon : styles.sun
+									}`}
+								/>
+								<div>
+									<div />
 								</div>
-								<span>{darkTheme ? 'Dark' : 'Light'} mode</span>
-							</button>
-						</li>
-						<li>
-							<button onClick={handleActiveDropdown}>
-								<span className={`${imageStyles.icon} ${styles.account}`} />
-								Account
-							</button>
-						</li>
-					</ul>
-				</nav>
-				{activeDropdown && (
-					<>
-						<Dropdown
-							user={user}
-							darkTheme={darkTheme}
-							onColorTheme={onColorTheme}
-						/>
-						<div
-							className={styles['transparent-background']}
-							onClick={handleCloseDropdown}
-							data-testid="transparentBgc"
-						/>
-					</>
-				)}
-			</header>
-		</>
+							</div>
+							<span>{darkTheme ? 'Dark' : 'Light'} mode</span>
+						</button>
+					</li>
+					<li>
+						<button onClick={handleActiveDropdown}>
+							<span className={`${imageStyles.icon} ${styles.account}`} />
+							Account
+						</button>
+					</li>
+				</ul>
+			</nav>
+			{activeDropdown && (
+				<>
+					<Dropdown
+						user={user}
+						darkTheme={darkTheme}
+						onColorTheme={onColorTheme}
+					/>
+					<div
+						className={styles['transparent-background']}
+						onClick={handleCloseDropdown}
+						data-testid="transparentBgc"
+					/>
+				</>
+			)}
+		</header>
 	);
 };
 Header.propTypes = {
