6a7e8298e2d6a2be91bb1cb0cbaada11b79ebdee
Weiss Bai <whitesgr03@gmail.com>
2025-03-18 17:33:37 +0800

    chore: added the dev server's headers

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,19 @@ export default defineConfig({
 	server: {
 		open: '/posts',
 		port: 7001,
+		headers: {
+			'X-Content-Type-Options': ' nosniff',
+			// 'Cross-Origin-Opener-Policy': 'same-origin',
+			'Cross-Origin-Embedder-Policy': 'credentialless',
+			'X-Frame-Options': 'DENY',
+			'Cross-Origin-Resource-Policy': 'same-origin',
+			'Referrer-Policy': 'no-referrer',
+			'X-XSS-Protection': '0',
+			'X-DNS-Prefetch-Control': 'off',
+			'Strict-Transport-Security':
+				'max-age=63072000; includeSubDomains preload',
+			'Content-Security-Policy-Report-Only': `default-src 'none'; connect-src 'self' localhost:3000 *.tiny.cloud; script-src 'self' *.tiny.cloud 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com/css2 *.tiny.cloud; font-src https:; img-src 'self' https: data:; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests`,
+		},
 	},
 	test: {
 		globals: true,
