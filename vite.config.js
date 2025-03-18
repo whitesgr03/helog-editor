e60bf819c7ca90ef80065f4e3cd8d23170051ef7
Weiss Bai <whitesgr03@gmail.com>
2025-03-18 12:01:25 +0800

    chore: removed csp nonce option

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { randomBytes } from 'node:crypto';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	html: {
-		cspNonce: randomBytes(16).toString('base64'),
-	},
 	server: {
 		open: '/posts',
 		port: 7001,
