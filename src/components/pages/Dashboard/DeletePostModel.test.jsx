7b242988b0a645cf94615735f83776e5918a9b04
Weiss Bai <whitesgr03@gmail.com>
2025-05-31 23:33:11 +0800

    test: added the react query module and mock the useAppDataAPI hook to all tests

--- src/components/pages/Dashboard/DeletePostModel.test.jsx
+++ src/components/pages/Dashboard/DeletePostModel.test.jsx
@@ -1,90 +1,124 @@
 import { vi, describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { DeletePostModel } from './DeletePostModel';
 import { Loading } from '../../utils/Loading';
 
 import { deletePost } from '../../../utils/handlePost';
+import { useAppDataAPI } from '../App/AppContext';
 
-vi.mock('../../../components/utils/Loading');
+vi.mock('../App/AppContext');
+vi.mock('../../utils/Loading');
 vi.mock('../../../utils/handlePost');
 
 describe('DeletePostModel component', () => {
 	it('should render an error message alert if delete a post fails', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			id: 'test',
+			postId: '0',
+			title: 'post',
+		};
+
+		const mockCustomHook = {
 			onAlert: vi.fn(),
-			onActiveModal: vi.fn(),
+			onModal: vi.fn(),
 		};
-		const mockFetchResult = {
-			success: false,
-		};
-
-		deletePost.mockResolvedValueOnce(mockFetchResult);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
-
-		render(<DeletePostModel {...mockProps} />);
+		vi.mocked(deletePost).mockImplementationOnce(
+			() => new Promise((_r, reject) => setTimeout(() => reject(Error()), 300)),
+		);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
+		const queryClient = new QueryClient();
+		render(
+			<QueryClientProvider client={queryClient}>
+				<DeletePostModel {...mockProps} />
+			</QueryClientProvider>,
+		);
 
 		const button = screen.getByRole('button', { name: 'Delete' });
 
-		user.click(button);
+		await user.click(button);
 
-		const loadingComponent = await screen.findByText('Loading component');
+		await waitForElementToBeRemoved(() =>
+			screen.getByText('Loading component'),
+		);
 
 		expect(deletePost).toBeCalledTimes(1);
-		expect(mockProps.onAlert).toBeCalledTimes(1);
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(2);
 	});
 	it('should close modal if the cancel button is clicked', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			onActiveModal: vi.fn(),
+			postId: '0',
+			title: 'post',
 		};
-
-		render(<DeletePostModel {...mockProps} />);
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+		const queryClient = new QueryClient();
+		render(
+			<QueryClientProvider client={queryClient}>
+				<DeletePostModel {...mockProps} />
+			</QueryClientProvider>,
+		);
 
 		const closeButton = screen.getByRole('button', { name: 'Cancel' });
 
 		await user.click(closeButton);
 
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(1);
 	});
 	it('should delete a post if the delete button is clicked', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			id: '0',
-			title: 'target-title',
-			onDeletePost: vi.fn(),
-			onAlert: vi.fn(),
-			onActiveModal: vi.fn(),
+			postId: '0',
+			title: 'post',
 		};
+
 		const mockFetchResult = {
 			success: true,
 			data: {},
 		};
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(deletePost).mockResolvedValueOnce(mockFetchResult);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
 
-		deletePost.mockResolvedValueOnce(mockFetchResult);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
-
-		render(<DeletePostModel {...mockProps} />);
-
+		const queryClient = new QueryClient();
+		queryClient.setQueryData(['userPosts'], {
+			pages: [{ data: { userPosts: [{ _id: mockProps.postId }] } }],
+			pageParams: {},
+		});
+		render(
+			<QueryClientProvider client={queryClient}>
+				<DeletePostModel {...mockProps} />
+			</QueryClientProvider>,
+		);
 		const button = screen.getByRole('button', { name: 'Delete' });
 		const title = screen.getByText(mockProps.title);
 
-		user.click(button);
-
-		const loadingComponent = await screen.findByText('Loading component');
+		await user.click(button);
 
 		expect(title).toBeInTheDocument();
 		expect(deletePost).toBeCalledTimes(1);
-		expect(mockProps.onAlert).toBeCalledTimes(1);
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
-		expect(mockProps.onDeletePost)
-			.toBeCalledWith(mockProps.id)
-			.toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(2);
 	});
 });
