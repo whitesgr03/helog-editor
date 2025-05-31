b2d4f7a3398da45b4ab0ee0fafcae10c69a6b408
Weiss Bai <whitesgr03@gmail.com>
2025-05-31 15:01:24 +0800

    test: added the react query module and mock the useAppDataAPI hook to all tests

--- src/components/pages/App/Alert.test.jsx
+++ src/components/pages/App/Alert.test.jsx
@@ -4,59 +4,95 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Alert } from './Alert';
+import { useAlert, useAppDataAPI } from './AppContext';
+
+vi.mock('./AppContext');
 
 describe('Alert component', () => {
-	it(`should render no alert message if the alert prop is empty`, () => {
-		const mockProps = {
-			alert: [],
-		};
-		render(<Alert {...mockProps} />);
-
-		const alert = screen.getByTestId('alert');
-		const message = screen.getByTestId('message');
-
-		expect(alert)
-			.not.toHaveClass(/active/)
-			.not.toHaveClass(/error/);
-		expect(message).not.toHaveTextContent();
-	});
-	it(`should render the alert message if the alert prop is provided`, () => {
-		const mockProps = {
-			alert: [{ message: 'alert-message', error: true, delay: 100 }],
+	it(`should render no alert message if the alert data is empty`, () => {
+		const mockAlertData = [];
+		const mockCustomHook = {
 			onAlert: vi.fn(),
+			onModal: vi.fn(),
 		};
-		render(<Alert {...mockProps} />);
+
+		vi.mocked(useAlert).mockReturnValue(mockAlertData);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		render(<Alert />);
+
+		const alert = screen.getByTestId('alert');
+
+		expect(alert).not.toHaveClass(/active/);
+		expect(alert).not.toHaveClass(/error/);
+	});
+	it(`should render the alert message if the alert data is provided`, () => {
+		const mockAlertData = [
+			{ message: 'alert-message', error: true, delay: 100 },
+		];
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+
+		vi.mocked(useAlert).mockReturnValue(mockAlertData);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		render(<Alert />);
 
 		const alert = screen.getByTestId('alert');
 		const message = screen.getByTestId('message');
 
-		expect(alert)
-			.toHaveClass(/active/)
-			.toHaveClass(/error/);
-		expect(message).toHaveTextContent(mockProps.alert[0].message);
+		expect(alert).toHaveClass(/active/);
+		expect(alert).toHaveClass(/error/);
+		expect(message).toHaveTextContent(mockAlertData[0].message);
 	});
 	it(`should remove the alert message if the alert message is expires`, async () => {
-		const mockProps = {
-			alert: [{ message: 'alert-message', error: false, delay: 100 }],
+		let mockAlertData = [{ message: 'alert-message', error: true, delay: 100 }];
+		const mockCustomHook = {
 			onAlert: vi.fn(),
+			onModal: vi.fn(),
 		};
-		render(<Alert {...mockProps} />);
+
+		vi.mocked(useAlert).mockReturnValue(mockAlertData);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		render(<Alert />);
 
 		const alert = screen.getByTestId('alert');
+		const message = screen.getByTestId('message');
 
 		fireEvent.transitionEnd(alert);
 
+		expect(alert).toHaveClass(/active/);
+		expect(message).toHaveTextContent(mockAlertData[0].message);
+
 		await waitFor(() => {
-			expect(mockProps.onAlert).toBeCalledWith([]).toBeCalledTimes(1);
+			expect(mockCustomHook.onAlert).toBeCalledWith([]);
+			expect(mockCustomHook.onAlert).toBeCalledTimes(1);
+			mockAlertData.pop();
 		});
+
+		fireEvent.transitionEnd(alert);
+
+		expect(alert).not.toHaveClass(/active/);
+		expect(alert).not.toHaveClass(/error/);
+		expect(message).toHaveTextContent('');
 	});
 	it(`should pause the alert timer if the user mouse over to alert element`, async () => {
 		const user = userEvent.setup();
-		const mockProps = {
-			alert: [{ message: 'alert-message', error: false, delay: 100 }],
+		const mockAlertData = [
+			{ message: 'alert-message', error: true, delay: 100 },
+		];
+		const mockCustomHook = {
 			onAlert: vi.fn(),
+			onModal: vi.fn(),
 		};
-		render(<Alert {...mockProps} />);
+
+		vi.mocked(useAlert).mockReturnValue(mockAlertData);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		render(<Alert />);
 
 		const alert = screen.getByTestId('alert');
 		const message = screen.getByTestId('message');
@@ -65,47 +101,52 @@ describe('Alert component', () => {
 
 		user.hover(message);
 
-		expect(mockProps.onAlert).toBeCalledTimes(0);
+		expect(mockCustomHook.onAlert).toBeCalledTimes(0);
 
 		user.hover(alert);
 
-		expect(mockProps.onAlert).toBeCalledTimes(0);
+		expect(mockCustomHook.onAlert).toBeCalledTimes(0);
 
 		user.unhover(alert);
 
 		await waitFor(() => {
-			expect(mockProps.onAlert).toBeCalledWith([]).toBeCalledTimes(1);
+			expect(mockCustomHook.onAlert).toBeCalledWith([]);
+			expect(mockCustomHook.onAlert).toBeCalledTimes(1);
 		});
 	});
 	it(`should render the second alert message if a second alert message is added`, async () => {
-		let mockProps = {
-			alert: [{ message: 'first-message', error: false, delay: 100 }],
+		let mockAlertData = [{ message: 'alert-message', error: true, delay: 100 }];
+		const mockCustomHook = {
 			onAlert: vi.fn(),
+			onModal: vi.fn(),
 		};
-		const { rerender } = render(<Alert {...mockProps} />);
+
+		vi.mocked(useAlert).mockReturnValue(mockAlertData);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const { rerender } = render(<Alert />);
 
 		const alert = screen.getByTestId('alert');
 		const message = screen.getByTestId('message');
 
-		expect(message).toHaveTextContent(mockProps.alert[0].message);
+		expect(message).toHaveTextContent(mockAlertData[0].message);
 		expect(alert).toHaveClass(/active/);
 
-		mockProps.alert.push({
+		mockAlertData.push({
 			message: 'second-message',
 			error: true,
 			delay: 100,
 		});
 
-		rerender(<Alert {...mockProps} />);
+		rerender(<Alert />);
 
 		expect(alert).not.toHaveClass(/active/);
 
 		fireEvent.transitionEnd(alert);
 
 		await waitFor(() => {
-			expect(mockProps.onAlert)
-				.toBeCalledWith([mockProps.alert[1]])
-				.toBeCalledTimes(1);
+			expect(mockCustomHook.onAlert).toBeCalledWith([mockAlertData[1]]);
+			expect(mockCustomHook.onAlert).toBeCalledTimes(1);
 		});
 	});
 });
