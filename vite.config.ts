e254ba874196bb6dc037d3560ef6d8d7d283cfc1
Weiss Bai <whitesgr03@gmail.com>
2026-02-04 18:43:49 +0800

    chore: run dev script without opening app in browser

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		open: '/posts',
 		port: 7001,
 		headers: {
 			'X-Content-Type-Options': ' nosniff',
