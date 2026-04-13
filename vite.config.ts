cf69ed4f99e483e6a16f6f1e0e495d3339173546
Weiss Bai <whitesgr03@gmail.com>
2026-04-13 12:36:29 +0800

    chore: add preview property

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+	preview: {
+		port: 7001,
+		headers: {
+			'X-Content-Type-Options': ' nosniff',
+			'Cross-Origin-Embedder-Policy': 'credentialless',
+			'X-Frame-Options': 'DENY',
+			'Cross-Origin-Resource-Policy': 'same-origin',
+			'Referrer-Policy': 'no-referrer',
+			'X-XSS-Protection': '0',
+			'X-DNS-Prefetch-Control': 'off',
+			'Strict-Transport-Security':
+				'max-age=63072000; includeSubDomains preload',
+			'Content-Security-Policy-Report-Only': `default-src 'none'; connect-src 'self' localhost:4000 *.tiny.cloud; script-src 'self' *.tiny.cloud 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com/css2 *.tiny.cloud; font-src https:; img-src 'self' https: data:; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests`,
+		},
+	},
 	server: {
 		port: 7001,
 		headers: {
